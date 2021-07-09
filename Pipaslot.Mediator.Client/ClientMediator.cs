955acc05e6bad370c41abbcd1b647167479ff85b
Petr Stipek <petr.stipek@bitbee.ch>
2021-07-09 10:31:19 +0200

    get rid of concurrency handling

--- Pipaslot.Mediator.Client/ClientMediator.cs
+++ Pipaslot.Mediator.Client/ClientMediator.cs
@@ -1,7 +1,6 @@
 ﻿using Pipaslot.Mediator.Abstractions;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -18,9 +17,6 @@ namespace Pipaslot.Mediator.Client
         private readonly HttpClient _httpClient;
         private readonly ILogger<ClientMediator> _logger;
 
-        private readonly Dictionary<int, Task> _queryTaskCache = new Dictionary<int, Task>();
-        private readonly object _queryTaskCacheLock = new object();
-
         public ClientMediator(HttpClient httpClient, ILogger<ClientMediator> logger)
         {
             _httpClient = httpClient;
@@ -32,19 +28,8 @@ namespace Pipaslot.Mediator.Client
             var contract = CreateContract(request);
             var requestType = request.GetType();
 
-            var hashCode = (contract.Json, contract.ObjectName).GetHashCode();
-            try
-            {
-                var task = GetRequestTaskFromCacheOrCreateNewRequest<object>(hashCode, contract, requestType, cancellationToken);
-                return await task;
-            }
-            finally
-            {
-                lock (_queryTaskCacheLock)
-                {
-                    _queryTaskCache.Remove(hashCode);
-                }
-            }
+            var task = SendRequest<object>(contract, requestType, cancellationToken);
+            return await task;
         }
 
         public async Task<IMediatorResponse<TResponse>> Execute<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
@@ -52,50 +37,31 @@ namespace Pipaslot.Mediator.Client
             var contract = CreateContract(request);
             var requestType = request.GetType();
 
-            var hashCode = (contract.Json, contract.ObjectName).GetHashCode();
-            try
-            {
-                var task = GetRequestTaskFromCacheOrCreateNewRequest<TResponse>(hashCode, contract, requestType, cancellationToken);
-                return await task;
-            }
-            finally
-            {
-                lock (_queryTaskCacheLock)
-                {
-                    _queryTaskCache.Remove(hashCode);
-                }
-            }
-        }
-
-        private Task<IMediatorResponse<TResponse>> GetRequestTaskFromCacheOrCreateNewRequest<TResponse>(int hashCode, MediatorRequestSerializable contract, Type requestType, CancellationToken cancellationToken = default)
-        {
-            lock (_queryTaskCacheLock)
-            {
-                if (_queryTaskCache.TryGetValue(hashCode, out var task))
-                {
-                    return (Task<IMediatorResponse<TResponse>>)task;
-                }
-                var newTask = SendRequest<TResponse>(contract, requestType, cancellationToken);
-                _queryTaskCache[hashCode] = newTask;
-                return newTask;
-            }
+            var task = SendRequest<TResponse>(contract, requestType, cancellationToken);
+            return await task;
         }
 
         private async Task<IMediatorResponse<TResponse>> SendRequest<TResponse>(MediatorRequestSerializable contract, Type requestType, CancellationToken cancellationToken = default)
+        {
+            var (result, response) = await CallSendRequest<TResponse>(contract, requestType, cancellationToken);
+            return await OnRequestPassed<TResponse>(result, response);
+        }
+
+        private async Task<(IMediatorResponse<TResponse>, HttpResponseMessage)> CallSendRequest<TResponse>(MediatorRequestSerializable contract, Type requestType, CancellationToken cancellationToken = default)
         {
             try
             {
                 var url = MediatorRequestSerializable.Endpoint + $"?type={requestType}";
                 var response = await _httpClient.PostAsJsonAsync(url, contract, cancellationToken);
                 response.EnsureSuccessStatusCode();
-
                 var result = await response.Content.ReadFromJsonAsync<MediatorResponseDeserialized<TResponse>>(cancellationToken: cancellationToken);
-                return result?? throw new InvalidOperationException("No data received");
+                return (result, response);
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "Mediator request failed for "+ requestType);
-                return new MediatorResponse<TResponse>(e.Message);
+                await OnRequestFailed(e, contract, requestType);
+
+                return (new MediatorResponse<TResponse>(e.Message), null);
             }
         }
 
@@ -109,6 +75,19 @@ namespace Pipaslot.Mediator.Client
                 }),
                 ObjectName = request.GetType().AssemblyQualifiedName
             };
+        }
+
+        protected virtual Task<IMediatorResponse<TResponse>> OnRequestPassed<TResponse>(IMediatorResponse<TResponse> result, HttpResponseMessage response)
+        {
+            IMediatorResponse<TResponse> normalized = result ?? throw new InvalidOperationException("No data received");
+            return Task.FromResult(normalized);
+        }
+
+        protected virtual Task OnRequestFailed(Exception e, MediatorRequestSerializable contract, Type requestType)
+        {
+
+            _logger.LogError(e, "Mediator request failed for " + requestType);
+            return Task.CompletedTask;
         }
     }
 }
