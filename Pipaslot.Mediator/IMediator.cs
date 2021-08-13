58c08bb6a4f5b29a35f5b1c743450522d680e4f5
Petr Stipek <petr.stipek@bitbee.ch>
2021-08-14 00:57:42 +0200

    update annotation

--- Pipaslot.Mediator/IMediator.cs
+++ Pipaslot.Mediator/IMediator.cs
@@ -11,11 +11,18 @@ namespace Pipaslot.Mediator
         /// <summary>
         /// Execute action and wait for response data
         /// </summary>
-        Task<IMediatorResponse<TResponse>> Execute<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+        /// <typeparam name="TResult">Result object type returned from handler</typeparam>
+        /// <param name="request">Object managing input parameters</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Returns wrapper managing response state and data</returns>
+        Task<IMediatorResponse<TResult>> Execute<TResult>(IRequest<TResult> request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Send message without feedback expectation
         /// </summary>
+        /// <param name="message">Object managing input parameters</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Returns wrapper managing response state</returns>
         Task<IMediatorResponse> Dispatch(IMessage message, CancellationToken cancellationToken = default);
     }
 }
