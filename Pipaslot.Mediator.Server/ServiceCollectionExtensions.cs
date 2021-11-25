e68ab3fee1db4aa09d3b7d7f595684fa06c3b610
Petr Stipek <petr.stipek@bitbee.ch>
2021-11-25 13:27:13 +0100

    refactor

--- Pipaslot.Mediator.Server/ServiceCollectionExtensions.cs
+++ Pipaslot.Mediator.Server/ServiceCollectionExtensions.cs
@@ -1,28 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Pipaslot.Mediator.Configuration;
+using Pipaslot.Mediator.Serialization;
 using System;
 
 namespace Pipaslot.Mediator.Server
 {
     public static class ServiceCollectionExtensions
     {
-        /// <summary>
-        /// Configures handler sources and pipeline for handler processing.
-        /// Every Request/Message is configured to have exactly one handler by default.
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="configure">Mediator configuration</param>
-        /// <returns></returns>
-        [Obsolete("Use AddMediatorServer() instead. Otherwise some important dependencies wont be registered")]
-        public static IPipelineConfigurator AddMediator(this IServiceCollection services, Action<ServerMediatorOptions> configure)
-        {
-            var options = new ServerMediatorOptions();
-            configure(options);
-            services.AddSingleton(options);
-
-            return services.AddMediator();
-        }
-
         /// <summary>
         /// Configures handler sources and pipeline for handler processing.
         /// Every Request/Message is configured to have exactly one handler by default.
@@ -46,6 +30,7 @@ namespace Pipaslot.Mediator.Server
             var options = new ServerMediatorOptions();
             configure(options);
             services.AddSingleton(options);
+            services.AddSingleton<IContractSerializer, ContractSerializer>();
 
             return services.AddMediator();
         }
