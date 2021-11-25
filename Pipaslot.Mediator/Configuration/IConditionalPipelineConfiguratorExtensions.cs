3c7bf6713a91ed22b3da20aa948a3d47faf73704
Petr Stipek <petr.stipek@bitbee.ch>
2021-11-26 00:46:05 +0100

    remove lifetime modifier from execution middleware which has to be scoped

--- Pipaslot.Mediator/Configuration/IConditionalPipelineConfiguratorExtensions.cs
+++ Pipaslot.Mediator/Configuration/IConditionalPipelineConfiguratorExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Pipaslot.Mediator.Middlewares;
+﻿using Pipaslot.Mediator.Middlewares;
 
 namespace Pipaslot.Mediator
 {
@@ -8,18 +7,18 @@ namespace Pipaslot.Mediator
         /// <summary>
         /// Middleware running all handlers concurrently. Not further middleware will be executed after this one.
         /// </summary>
-        public static IConditionalPipelineConfigurator UseConcurrentMultiHandler(this IConditionalPipelineConfigurator config, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        public static IConditionalPipelineConfigurator UseConcurrentMultiHandler(this IConditionalPipelineConfigurator config)
         {
-            return config.Use<MultiHandlerConcurrentExecutionMiddleware>(lifetime);
+            return config.Use<MultiHandlerConcurrentExecutionMiddleware>();
         }
 
         /// <summary>
         /// Pipeline running all handlers in sequence one by one. Not further middleware will be executed after this one.
         /// For order specification see <see cref="ISequenceHandler"/>
         /// </summary>
-        public static IConditionalPipelineConfigurator UseSequenceMultiHandler(this IConditionalPipelineConfigurator config, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        public static IConditionalPipelineConfigurator UseSequenceMultiHandler(this IConditionalPipelineConfigurator config)
         {
-            return config.Use<MultiHandlerSequenceExecutionMiddleware>(lifetime);
+            return config.Use<MultiHandlerSequenceExecutionMiddleware>();
         }
     }
 }
