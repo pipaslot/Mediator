0559e71de381ae135786241e31e389d92b5ce0b9
Petr Stipek <petr.stipek@bitbee.ch>
2022-02-12 00:11:41 +0100

    update demo

---
+++ Demo/Server/Handlers/MessageWithNotificationHandler.cs
@@ -0,0 +1,22 @@
+﻿using Pipaslot.Mediator;
+using Demo.Shared.Requests;
+using Pipaslot.Mediator.Notifications;
+
+namespace Demo.Server.Handlers
+{
+    public class MessageWithNotificationHandler : IMessageHandler<MessageWithNotification>
+    {
+        private readonly INotificationProvider _notificationProvider;
+
+        public MessageWithNotificationHandler(INotificationProvider notificationProvider)
+        {
+            _notificationProvider = notificationProvider;
+        }
+
+        public Task Handle(MessageWithNotification action, CancellationToken cancellationToken)
+        {
+            _notificationProvider.AddSuccess("Message was accepted.", "Hi there");
+            return Task.CompletedTask;
+        }
+    }
+}
