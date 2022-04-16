519a986da791c971c9c6253c437382095c6a72fc
Petr Stipek <petr.stipek@bitbee.ch>
2022-04-16 22:48:10 +0200

    refactoring

---
+++ Pipaslot.Mediator.Http/Serialization/Converters/MediatorActionConvert.cs
@@ -0,0 +1,78 @@
+﻿using Pipaslot.Mediator.Http.Configuration;
+using System;
+using System.Text.Json;
+
+namespace Pipaslot.Mediator.Http.Serialization.Converters
+{
+    internal static class MediatorActionConvert
+    {
+        internal static object Read(
+            ref Utf8JsonReader reader,
+            JsonSerializerOptions options,
+            ICredibleActionProvider credibleActions,
+            out string typeValue)
+        {
+            Utf8JsonReader readerClone = reader;
+            if (readerClone.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException("StartObject was expected");
+            }
+
+            readerClone.Read();
+            if (readerClone.TokenType != JsonTokenType.PropertyName)
+            {
+                throw new JsonException("Property was expected");
+            }
+
+            var propertyName = readerClone.GetString();
+            if (propertyName != "$type")
+            {
+                throw new JsonException("Property with name $type was expected");
+            }
+            readerClone.Read();
+            if (readerClone.TokenType != JsonTokenType.String)
+            {
+                throw new JsonException("Value was expected");
+            }
+            typeValue = readerClone.GetString() ?? "";
+            if (typeValue == null)
+            {
+                throw new JsonException("Type value can not be null");
+            }
+            var actionType = ContractSerializerTypeHelper.GetType(typeValue);
+            credibleActions.VerifyCredibility(actionType);
+
+            var result = JsonSerializer.Deserialize(ref reader, actionType, options);
+            if (result == null)
+            {
+                throw new Exception($"Can not deserialize contract as type {typeValue} received from server");
+            }
+            return result;
+        }
+
+        internal static void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        {
+            switch (value)
+            {
+                case null:
+                    JsonSerializer.Serialize(writer, (object)null, options);
+                    break;
+                default:
+                    {
+                        var type = value.GetType();
+                        using var jsonDocument = JsonDocument.Parse(JsonSerializer.Serialize(value, type, options));
+                        writer.WriteStartObject();
+                        writer.WriteString("$type", ContractSerializerTypeHelper.GetIdentifier(type));
+
+                        foreach (var element in jsonDocument.RootElement.EnumerateObject())
+                        {
+                            element.WriteTo(writer);
+                        }
+
+                        writer.WriteEndObject();
+                        break;
+                    }
+            }
+        }
+    }
+}
--- Pipaslot.Mediator.Http/Serialization/Converters/SimpleContractSerializableConverter.cs
+++ Pipaslot.Mediator.Http/Serialization/Converters/SimpleContractSerializableConverter.cs
@@ -18,72 +18,13 @@ namespace Pipaslot.Mediator.Http.Serialization.Converters
 
         public override ContractSerializable? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            Utf8JsonReader readerClone = reader;
-            if (readerClone.TokenType != JsonTokenType.StartObject)
-            {
-                throw new JsonException("StartObject was expected");
-            }
-
-            readerClone.Read();
-            if (readerClone.TokenType != JsonTokenType.PropertyName)
-            {
-                throw new JsonException("Property was expected");
-            }
-
-            var propertyName = readerClone.GetString();
-            if (propertyName != "$type")
-            {
-                throw new JsonException("Property with name $type was expected");
-            }
-            readerClone.Read();
-            if (readerClone.TokenType != JsonTokenType.String)
-            {
-                throw new JsonException("Value was expected");
-            }
-            var typeValue = readerClone.GetString();
-            if (typeValue == null)
-            {
-                throw new JsonException("Type value can not be null");
-            }
-            return CreateObject(ref reader, typeValue, options);
-        }
-
-        private ContractSerializable CreateObject(ref Utf8JsonReader reader, string type, JsonSerializerOptions options)
-        {
-            var actionType = ContractSerializerTypeHelper.GetType(type);
-            _credibleActions.VerifyCredibility(actionType);
-
-            var result = JsonSerializer.Deserialize(ref reader, actionType, options);
-            if (result == null)
-            {
-                throw new Exception($"Can not deserialize contract as type {type} received from server");
-            }
-            return new ContractSerializable(result, type);
+            var action = MediatorActionConvert.Read(ref reader, options, _credibleActions, out var typeValue);
+            return new ContractSerializable(action, typeValue);
         }
 
         public override void Write(Utf8JsonWriter writer, ContractSerializable value, JsonSerializerOptions options)
         {
-            switch (value.Content)
-            {
-                case null:
-                    JsonSerializer.Serialize(writer, (ContractSerializable)null, FullJsonContractSerializer.SerializationOptionsWithoutConverters);
-                    break;
-                default:
-                    {
-                        var type = value.Content.GetType();
-                        using var jsonDocument = JsonDocument.Parse(JsonSerializer.Serialize(value.Content, type, FullJsonContractSerializer.SerializationOptionsWithoutConverters));
-                        writer.WriteStartObject();
-                        writer.WriteString("$type", ContractSerializerTypeHelper.GetIdentifier(type));
-
-                        foreach (var element in jsonDocument.RootElement.EnumerateObject())
-                        {
-                            element.WriteTo(writer);
-                        }
-
-                        writer.WriteEndObject();
-                        break;
-                    }
-            }
+            MediatorActionConvert.Write(writer, value.Content, options);
         }
     }
 }
