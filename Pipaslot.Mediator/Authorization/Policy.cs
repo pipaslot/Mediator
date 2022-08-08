33d96a7ad73fe0783f5b23c52be8882c0761c78b
Petr Stipek <petr.stipek@bitbee.ch>
2022-08-09 00:31:20 +0200

    logical policy operatopolicy logical operator helpers

---
+++ Pipaslot.Mediator/Authorization/Policy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pipaslot.Mediator.Authorization
+{
+    /// <summary>
+    /// Helper class to combine multiple policies via logical operators
+    /// </summary>
+    public sealed class Policy : List<IPolicy>, IPolicy
+    {
+        public PolicyOperator Operator { get; }
+
+        public Policy(PolicyOperator @operator)
+        {
+            Operator = @operator;
+        }
+        
+        public Policy(PolicyOperator @operator, params IPolicy[] policies) : base(policies)
+        {
+            Operator = @operator;
+        }
+
+        /// <summary>
+        /// Combine multiple policies together with AND operator
+        /// </summary>
+        public static Policy And(params IPolicy[] policies)
+        {
+            var expression = new Policy(PolicyOperator.And);
+            expression.AddRange(policies);
+            return expression;
+        }
+
+        /// <summary>
+        /// Combine multiple policies together with OR operator
+        /// </summary>
+        public static Policy Or(params IPolicy[] policies)
+        {
+            var expression = new Policy(PolicyOperator.Or);
+            expression.AddRange(policies);
+            return expression;
+        }
+
+        public async Task<IRuleSet> Resolve(IServiceProvider services, CancellationToken cancellationToken)
+        {
+            var res = new RuleSetCollection(Operator);
+            foreach(var policy in this)
+            {
+                var ruleSet = await policy.Resolve(services, cancellationToken);
+                res.Add(ruleSet);
+            }
+            return res;
+        }
+    }
+}
---
+++ Pipaslot.Mediator/Authorization/PolicyExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Pipaslot.Mediator.Authorization
+{
+    public static class PolicyExtensions
+    {
+        /// <summary>
+        /// Combine multiple policies together with AND operator
+        /// </summary>
+        public static Policy And(this IPolicy policy, params IPolicy[] andPolicies)
+        {
+            var expression = new Policy(PolicyOperator.And);
+            expression.Add(policy);
+            expression.AddRange(andPolicies);
+            return expression;
+        }
+
+        /// <summary>
+        /// Combine multiple policies together with OR operator
+        /// </summary>
+        public static Policy Or(this IPolicy policy, params IPolicy[] orPolicies)
+        {
+            var expression = new Policy(PolicyOperator.Or);
+            expression.Add(policy);
+            expression.AddRange(orPolicies);
+            return expression;
+
+        }
+    }
+}
