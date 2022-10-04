9befefad2960abccd459c3d8d874691bc8598da1
Petr Stipek <petr.stipek@bitbee.ch>
2022-10-04 23:29:50 +0200

    separate classes

--- Demo/Shared/Auth/IAuthenticationFormMessage.cs
+++ Demo/Shared/Auth/IAuthenticationFormMessage.cs
@@ -1,6 +1,8 @@
-﻿namespace Demo.Shared.Auth
+﻿using Pipaslot.Mediator;
+
+namespace Demo.Shared.Auth
 {
-    public class ConditionalAuthenticationMessage : IAuthenticationFormMessage
+    public interface IAuthenticationFormMessage : IMessage
     {
         public bool RequireAuthentication { get; set; }
         public string RequiredRole { get; set; }
