428c8b2e82b7ebc743fd92e290150e598734884f
Petr Stipek <petr.stipek@bitbee.ch>
2022-12-21 14:15:13 +0100

    add long running request in demo

---
+++ Demo/Server/Handlers/Playground/LongRunningRequestHandler.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Shared.Playground;
+using Pipaslot.Mediator;
+
+namespace Demo.Server.Handlers.Playground
+{
+    public class LongRunningRequestHandler : IRequestHandler<LongRunningRequest, bool>
+    {
+        public async Task<bool> Handle(LongRunningRequest action, CancellationToken cancellationToken)
+        {
+            await Task.Delay(action.Seconds * 1000, cancellationToken);
+            return true;
+        }
+    }
+}
---
+++ Demo/Shared/Playground/LongRunningRequest.cs
@@ -0,0 +1,11 @@
+﻿using Pipaslot.Mediator;
+using Pipaslot.Mediator.Authorization;
+
+namespace Demo.Shared.Playground
+{
+    [AnonymousPolicy]
+    public record LongRunningRequest(int Seconds) : IRequest<bool>
+    {
+
+    }
+}
