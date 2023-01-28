8a1b8bc9b029591adfc4f5f7aa35a165c6a8f9da
Petr Stipek <petr.stipek@bitbee.ch>
2023-01-28 21:00:59 +0100

    refactoring

--- Pipaslot.Mediator.Http/ApplicationBuilderExtensions.cs
+++ Pipaslot.Mediator.Http/ApplicationBuilderExtensions.cs
@@ -4,7 +4,6 @@ using Pipaslot.Mediator.Http.Configuration;
 using Pipaslot.Mediator.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Pipaslot.Mediator.Http
 {
@@ -18,19 +17,12 @@ namespace Pipaslot.Mediator.Http
         /// <param name="checkExistingPolicies">Check that every action or action handler has at least one Authorization policy to prevent runtime exceptions</param>
         public static IApplicationBuilder UseMediator(this IApplicationBuilder app, bool checkMatchingHandlers = false, bool checkExistingPolicies = false)
         {
-            var options = app.ApplicationServices.GetService<ServerMediatorOptions>() ?? new ServerMediatorOptions();
-            app.UseMiddleware<MediatorMiddleware>(options);
-
-            if (checkMatchingHandlers || checkExistingPolicies)
+            RegisterMiddleware(app);
+            ExecuteChecker(app, new ExistenceCheckerSetting
             {
-                using var scope = app.ApplicationServices.CreateScope();
-                var checker = scope.ServiceProvider.GetRequiredService<IHandlerExistenceChecker>();
-                checker.Verify(new ExistenceCheckerSetting
-                {
-                    CheckMatchingHandlers = checkMatchingHandlers,
-                    CheckExistingPolicies = checkExistingPolicies
-                });
-            }
+                CheckMatchingHandlers = checkMatchingHandlers,
+                CheckExistingPolicies = checkExistingPolicies
+            });
             return app;
         }
 
@@ -42,21 +34,30 @@ namespace Pipaslot.Mediator.Http
         /// <param name="ignoredPolicyCheckTypes">If set, check that every action or action handler has at least one Authorization policy to prevent runtime exceptions. All the specified types will be ignored during the check.</param>
         public static IApplicationBuilder UseMediator(this IApplicationBuilder app, bool checkMatchingHandlers, params Type[] ignoredPolicyCheckTypes)
         {
+            RegisterMiddleware(app);
+            ExecuteChecker(app, new ExistenceCheckerSetting
+            {
+                CheckMatchingHandlers = checkMatchingHandlers,
+                CheckExistingPolicies = true,
+                IgnoredPolicyChecks = new HashSet<Type>(ignoredPolicyCheckTypes)
+            });
+            return app;
+        }
+
+        private static void RegisterMiddleware(IApplicationBuilder app)
+        {
             var options = app.ApplicationServices.GetService<ServerMediatorOptions>() ?? new ServerMediatorOptions();
             app.UseMiddleware<MediatorMiddleware>(options);
+        }
 
-            if (checkMatchingHandlers || ignoredPolicyCheckTypes.Any())
+        private static void ExecuteChecker(IApplicationBuilder app, ExistenceCheckerSetting setting)
+        {
+            if (setting.CheckMatchingHandlers || setting.CheckExistingPolicies)
             {
                 using var scope = app.ApplicationServices.CreateScope();
                 var checker = scope.ServiceProvider.GetRequiredService<IHandlerExistenceChecker>();
-                checker.Verify(new ExistenceCheckerSetting
-                {
-                    CheckMatchingHandlers = checkMatchingHandlers,
-                    CheckExistingPolicies = true,
-                    IgnoredPolicyChecks = new HashSet<Type>(ignoredPolicyCheckTypes)
-                });
+                checker.Verify(setting);
             }
-            return app;
         }
     }
 }
