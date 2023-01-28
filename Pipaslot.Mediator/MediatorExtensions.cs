97dc2614e8917d0487c8698adab9ae34fa1af836
Petr Stipek <petr.stipek@bitbee.ch>
2023-01-28 23:47:52 +0100

    Add extension ExecuteOrDefault

--- Pipaslot.Mediator/MediatorExtensions.cs
+++ Pipaslot.Mediator/MediatorExtensions.cs
@@ -11,11 +11,22 @@ namespace Pipaslot.Mediator
         /// Execute action and return result if operation succeded otherwise returns default value for the result type
         /// </summary>
         /// <typeparam name="TResult">Action result type</typeparam>
-        /// <returns>Action result type of its defaut implementation</returns>
+        /// <returns>Action result type or its defaut implementation</returns>
         public static async Task<TResult?> ExecuteOrDefault<TResult>(this IMediator mediator, IMediatorAction<TResult> request, CancellationToken cancellationToken = default)
         {
             var response = await mediator.Execute(request, cancellationToken);
             return response.Success ? response.Result : default;
+        }
+
+        /// <summary>
+        /// Execute action and return result if operation succeded otherwise returns default value passed as second argument
+        /// </summary>
+        /// <typeparam name="TResult">Action result type</typeparam>
+        /// <returns>Action result type or default value</returns>
+        public static async Task<TResult> ExecuteOrDefault<TResult>(this IMediator mediator, IMediatorAction<TResult> request, TResult defaultValue, CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Execute(request, cancellationToken);
+            return response.Success ? response.Result : defaultValue;
         }
 
         /// <summary>
