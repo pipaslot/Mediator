06ad7c5bd5a2786383e4ff1ce63f2733bab8de8d
Petr Stipek <petr.stipek@bitbee.ch>
2023-02-12 10:17:47 +0100

    add configuration extensions

--- Pipaslot.Mediator/MediatorConfiguratorExtensions.cs
+++ Pipaslot.Mediator/MediatorConfiguratorExtensions.cs
@@ -1,17 +1,46 @@
 ﻿using Pipaslot.Mediator.Authorization;
 using Pipaslot.Mediator.Configuration;
 using System;
+using System.Linq;
 
 namespace Pipaslot.Mediator
 {
     public static class MediatorConfiguratorExtensions
     {
         /// <summary>
+        /// Register action-specific pipeline with separated middlewares applied only for specified action types.
+        /// </summary>
+        public static IMediatorConfigurator AddPipelineForActions(this IMediatorConfigurator configurator, Action<IMiddlewareRegistrator> subMiddlewares, params Type[] actionTypes)
+        {
+            var name = string.Join("-", actionTypes.Select(t => t.ToString()));
+            return configurator.AddPipeline(action =>
+                actionTypes.Any(mt => mt.IsAssignableFrom(action.GetType())),
+                subMiddlewares,
+                name);
+        }
+
+        /// <summary>
         /// Register action-specific pipeline with separated middlewares applied only for actions implementing TActionMarker.
         /// </summary>
         public static IMediatorConfigurator AddPipelineForAction<TActionMarker>(this IMediatorConfigurator configurator, Action<IMiddlewareRegistrator> subMiddlewares)
         {
-            return configurator.AddPipeline(action => typeof(TActionMarker).IsAssignableFrom(action.GetType()), subMiddlewares, nameof(TActionMarker));
+            return configurator.AddPipelineForActions(subMiddlewares, typeof(TActionMarker));
+        }
+
+        /// <summary>
+        /// Register action-specific pipeline with separated middlewares applied only for actions implementing any TActionMarker.
+        /// </summary>
+        public static IMediatorConfigurator AddPipelineForActions<TActionMarker1, TActionMarker2>(this IMediatorConfigurator configurator, Action<IMiddlewareRegistrator> subMiddlewares)
+        {
+            return configurator.AddPipelineForActions(subMiddlewares, typeof(TActionMarker1), typeof(TActionMarker2));
+        }
+
+        /// <summary>
+        /// Register action-specific pipeline with separated middlewares applied only for actions implementing any TActionMarker.
+        /// </summary>
+        public static IMediatorConfigurator AddPipelineForActions<TActionMarker1, TActionMarker2, TActionMarker3>(this IMediatorConfigurator configurator, Action<IMiddlewareRegistrator> subMiddlewares)
+        {
+            return configurator.AddPipelineForActions(subMiddlewares, typeof(TActionMarker1), typeof(TActionMarker2), typeof(TActionMarker3));
         }
 
         /// <summary>
