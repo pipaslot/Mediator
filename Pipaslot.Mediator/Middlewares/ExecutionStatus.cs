c63c9ec18212003afd40706cc289ce36a0437454
Petr Stipek <petr.stipek@bitbee.ch>
2023-04-17 23:09:49 +0200

    refactoring

--- Pipaslot.Mediator/Middlewares/ExecutionStatus.cs
+++ Pipaslot.Mediator/Middlewares/ExecutionStatus.cs
@@ -2,8 +2,19 @@
 {
     public enum ExecutionStatus
     {
+        /// <summary>
+        /// Mediator call will returns a success.
+        /// </summary>
         Succeeded = 0,
+
+        /// <summary>
+        /// Error was detected during processing. Mediator call will returns a failure.
+        /// </summary>
         Failed = 1,
+
+        /// <summary>
+        /// No handler was found in the handler execution middleware when it was expected. Mediator call will returns a failure
+        /// </summary>
         NoHandlerFound = 2,
     }
 }
