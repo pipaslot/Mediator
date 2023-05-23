5002283f2130b2279e7401f4cc69138b4c0ecc57
Petr Stipek <petr.stipek@bitbee.ch>
2023-05-24 00:40:04 +0200

    update doc

--- Pipaslot.Mediator/IScoped.cs
+++ Pipaslot.Mediator/IScoped.cs
@@ -1,7 +1,9 @@
-﻿namespace Pipaslot.Mediator
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Pipaslot.Mediator
 {
     /// <summary>
-    /// Handler marked with this interface will be registered with Scoped ServiceLifetime in Dependency Injection instead of Transient.
+    /// Handler marked with this interface will be registered with <see cref="ServiceLifetime.Scoped"/> in Dependency Injection instead of Transient.
     /// </summary>
     public interface IScoped
     {
--- Pipaslot.Mediator/ISingleton.cs
+++ Pipaslot.Mediator/ISingleton.cs
@@ -1,7 +1,9 @@
-﻿namespace Pipaslot.Mediator
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Pipaslot.Mediator
 {
     /// <summary>
-    /// Handler marked with this interface will be registered with Singleton ServiceLifetime in Dependency Injection instead of Transient.
+    /// Handler marked with this interface will be registered with <see cref="ServiceLifetime.Singleton"/> in Dependency Injection instead of Transient.
     /// </summary>
     public interface ISingleton
     {
