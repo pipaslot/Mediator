9441b84566f397999df2a9e74a8fe97ac8068feb
Petr Stipek <petr.stipek@bitbee.ch>
2023-07-23 22:09:39 +0200

    clean

--- Pipaslot.Mediator.Http/Internal/HttpExecutionEndpoint.cs
+++ Pipaslot.Mediator.Http/Internal/HttpExecutionEndpoint.cs
@@ -1,22 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
-using Pipaslot.Mediator.Http.Configuration;
-
-namespace Pipaslot.Mediator.Http.Internal
+﻿namespace Pipaslot.Mediator.Http.Internal
 {
     internal enum HttpExecutionEndpoint
 
     {
         /// <summary>
-        /// There is not HTTP request arround the Mediator execution. It was executed internally as background service
+        /// There is no HTTP request arround the Mediator execution. It was run internally as a background service.
         /// </summary>
         NoEndpoint = 0,
         /// <summary>
         /// The Mediator was started via HTTP, but only through its own dedicated endpoint.
         /// </summary>
-        MediatorEndpoint,
+        MediatorEndpoint = 1,
         /// <summary>
-        /// The mediator was started via HTTP through unknow Endpoint
+        /// The mediator was started via HTTP through unknow/custom Endpoint. 
         /// </summary>
-        CustomEndpoint
+        CustomEndpoint = 2
     }
 }
