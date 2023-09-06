3121c2bea03c3a81baed69ebfd5070cfa79e92e0
Petr Stipek <petr.stipek@bitbee.ch>
2023-09-06 21:08:23 +0200

    add comment

--- Pipaslot.Mediator/IMediatorContextAccessor.cs
+++ Pipaslot.Mediator/IMediatorContextAccessor.cs
@@ -19,6 +19,8 @@ namespace Pipaslot.Mediator
 
         /// <summary>
         /// All action context executed recursively
+        /// First is the root action
+        /// Last is the actual action
         /// </summary>
         IReadOnlyCollection<MediatorContext> ContextStack { get; }
     }
