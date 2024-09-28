d336753f085344689ff5bc31a981c8d2203dafb6
Petr Stipek <petr.stipek@bitbee.ch>
2024-09-28 10:53:54 +0200

    update comments

--- Pipaslot.Mediator/Authorization/Formatting/INode.cs
+++ Pipaslot.Mediator/Authorization/Formatting/INode.cs
@@ -1,6 +1,7 @@
 namespace Pipaslot.Mediator.Authorization.Formatting;
 
 /// <summary>
+/// This interface represents only abstraction to bound the inherited node kinds together.
 /// Represents conversion from <see cref="Rule"/> and <see cref="RuleSet"/> to a structure of nodes passed to <see cref="INodeFormatter"/>.
 /// The node tree is reduced to the relevant only depending on the <see cref="RuleSet.Operator"/>
 /// </summary>
