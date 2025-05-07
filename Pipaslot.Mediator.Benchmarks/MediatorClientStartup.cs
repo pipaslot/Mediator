e6b2d32f3d824009c6d5f9c6cf7521a72537a265
Petr Stipek <petr.stipek@bitbee.ch>
2025-05-07 22:28:19 +0200

    extend benchmark

--- Pipaslot.Mediator.Benchmarks/MediatorClientStartup.cs
+++ Pipaslot.Mediator.Benchmarks/MediatorClientStartup.cs
@@ -1,30 +1,35 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Microsoft.Extensions.DependencyInjection;
+using Pipaslot.Mediator.Benchmarks.Actions;
 using Pipaslot.Mediator.Http;
-using System.Reflection;
 
 namespace Pipaslot.Mediator.Benchmarks;
 
 /// <summary>
-/// Measure performance of the client side application initialization
+/// Measure performance impact of growing application to estimate/track the potential impact on the client side startup time
 /// </summary>
 [MemoryDiagnoser]
 public class MediatorClientStartup
 {
-    private readonly Assembly _assembly = typeof(Program).Assembly;
-
     [Benchmark(Baseline = true)]
-    public void MediatorWithNoAction()
-    {
-        var services = new ServiceCollection();
-        services.AddMediatorClient();
-    }
-
-    [Benchmark]
-    public void MediatorWith502Actions()
+    public void SingleAction()
     {
         var services = new ServiceCollection();
         services.AddMediatorClient()
-            .AddActionsFromAssembly(_assembly);
+            .AddActions([typeof(RequestAction)]);
+        
+        var mediator = services.BuildServiceProvider().GetRequiredService<IMediator>();
+        mediator.Execute(new RequestAction("Hello World"));
+    }
+
+    [Benchmark]
+    public void Containing502Actions()
+    {
+        var services = new ServiceCollection();
+        services.AddMediatorClient()
+            .AddActionsFromAssembly(typeof(Program).Assembly);
+        
+        var mediator = services.BuildServiceProvider().GetRequiredService<IMediator>();
+        mediator.Execute(new RequestAction("Hello World"));
     }
 }
