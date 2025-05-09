30109048a1ffbf7d7302c3af8ffe2a5ca6caa0fe
Petr Stipek <petr.stipek@bitbee.ch>
2025-05-09 23:58:04 +0200

    Refactor HandlerExecutionMiddleware to simplify executor retrieval logic

--- Pipaslot.Mediator/Middlewares/HandlerExecutionMiddleware.cs
+++ Pipaslot.Mediator/Middlewares/HandlerExecutionMiddleware.cs
@@ -10,27 +10,11 @@ namespace Pipaslot.Mediator.Middlewares;
 /// </summary>
 public class HandlerExecutionMiddleware(MediatorConfigurator configurator) : IExecutionMiddleware
 {
-
     public Task Invoke(MediatorContext context, MiddlewareDelegate next)
     {
-        var executor = context.HasActionReturnValue
-            ? GetRequestExecutor(context)
-            : GetMessageExecutor(context);
-
+        var actionType = context.Action.GetType();
+        var executorType = configurator.ReflectionCache.GetHandlerExecutorType(actionType);
+        var executor = (HandlerExecutor)context.Services.GetRequiredService(executorType);
         return executor.Execute(context);
-    }
-
-    private HandlerExecutor GetRequestExecutor(MediatorContext context)
-    {
-        var actionType = context.Action.GetType();
-        var executorType = configurator.ReflectionCache.GetHandlerExecutorType(actionType);
-        return (HandlerExecutor)context.Services.GetRequiredService(executorType);
-    }
-
-    private HandlerExecutor GetMessageExecutor(MediatorContext context)
-    {
-        var actionType = context.Action.GetType();
-        var executorType = configurator.ReflectionCache.GetHandlerExecutorType(actionType);
-        return (HandlerExecutor)context.Services.GetRequiredService(executorType);
     }
 }
