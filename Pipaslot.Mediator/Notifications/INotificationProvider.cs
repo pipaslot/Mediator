24e2910e23b609b361e166a4ab9a03fd88e7ffd5
Petr Stipek <petr.stipek@bitbee.ch>
2025-05-16 08:44:35 +0200

    add todo

--- Pipaslot.Mediator/Notifications/INotificationProvider.cs
+++ Pipaslot.Mediator/Notifications/INotificationProvider.cs
@@ -3,6 +3,7 @@
 /// <summary>
 /// Attach notification to action result collection
 /// </summary>
+/// TODO: The notification propagation between provider, receiver and receiver middleware is not completely clear. We need to prepare better approach
 public interface INotificationProvider
 {
     public void Add(Notification notification);
