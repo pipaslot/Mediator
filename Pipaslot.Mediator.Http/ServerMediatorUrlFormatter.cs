fc2ef695e51c4de0b6b8cef45402b72291816494
Petr Stipek <petr.stipek@bitbee.ch>
2025-06-01 08:28:18 +0200

    server side URL formatter

---
+++ Pipaslot.Mediator.Http/ServerMediatorUrlFormatter.cs
@@ -0,0 +1,18 @@
+﻿using Pipaslot.Mediator.Abstractions;
+using Pipaslot.Mediator.Http.Configuration;
+using Pipaslot.Mediator.Http.Serialization;
+using System.Net;
+
+namespace Pipaslot.Mediator.Http;
+
+public class ServerMediatorUrlFormatter(
+    ServerMediatorOptions options,
+    IContractSerializer serializer) : IMediatorUrlFormatter
+{
+    public string FormatHttpGet(IMediatorAction action)
+    {
+        var serialized = serializer.SerializeRequest(action).Json;
+        var decoded = WebUtility.UrlDecode(serialized);
+        return $"{options.Endpoint}?{MediatorConstants.ActionQueryParamName}={decoded}";
+    }
+}
