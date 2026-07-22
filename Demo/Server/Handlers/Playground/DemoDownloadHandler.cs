54d00aa6cc873bbfd42efccd5bfe4727eae1eb56
Petr Stipek <petr.stipek@bitbee.ch>
2026-07-22 22:48:14 +0200

    update demo

--- Demo/Server/Handlers/Playground/DemoDownloadHandler.cs
+++ Demo/Server/Handlers/Playground/DemoDownloadHandler.cs
@@ -1,20 +1,23 @@
 ﻿using Demo.Shared.Playground;
-using Pipaslot.Mediator;
+using Pipaslot.Mediator.Abstractions;
+using Pipaslot.Mediator.Http;
 
 namespace Demo.Server.Handlers.Playground;
 
-public class DemoDownloadHandler(IHttpContextAccessor httpContextAccessor) : IMessageHandler<DemoDownload>
+public class DemoDownloadHandler : IMediatorHandler<DemoDownload, IMediatorHttpResult>
 {
-    public async Task Handle(DemoDownload action, CancellationToken cancellationToken)
+    public Task<IMediatorHttpResult> Handle(DemoDownload action, CancellationToken cancellationToken)
     {
-        var httpContext = httpContextAccessor.HttpContext;
-        if (httpContext == null)
-        {
-            throw new InvalidOperationException($"Action {nameof(DemoDownload)} can be used only for request over HTTP.");
-        }
+        return Task.FromResult<IMediatorHttpResult>(new DemoDownloadResult(action.FileName, "Hello File!"));
+    }
+}
 
+internal class DemoDownloadResult(string fileName, string content) : IMediatorHttpResult
+{
+    public Task ApplyAsync(HttpContext httpContext, CancellationToken cancellationToken)
+    {
         httpContext.Response.ContentType = "text/plain";
-        httpContext.Response.Headers["Content-Disposition"] = $"attachment;filename={action.FileName}.txt";
-        await httpContext.Response.WriteAsync("Hello File!", cancellationToken);
+        httpContext.Response.Headers["Content-Disposition"] = $"attachment;filename={fileName}.txt";
+        return httpContext.Response.WriteAsync(content, cancellationToken);
     }
 }
--- Demo/Shared/Playground/DemoDownload.cs
+++ Demo/Shared/Playground/DemoDownload.cs
@@ -1,7 +1,8 @@
 ﻿using Pipaslot.Mediator;
 using Pipaslot.Mediator.Authorization;
+using Pipaslot.Mediator.Http;
 
 namespace Demo.Shared.Playground;
 
 [AnonymousPolicy]
-public record DemoDownload(string FileName) : IMessage;
+public record DemoDownload(string FileName) : IRequest<IMediatorHttpResult>;
