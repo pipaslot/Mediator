774dbec9e88e36a03b92552d55e4db82555682bc
Petr Stipek <petr.stipek@bitbee.ch>
2026-07-23 23:01:28 +0200

    improve demo

--- Demo/Server/Handlers/Playground/DemoDownloadHandler.cs
+++ Demo/Server/Handlers/Playground/DemoDownloadHandler.cs
@@ -1,23 +1,12 @@
 ﻿using Demo.Shared.Playground;
 using Pipaslot.Mediator.Abstractions;
-using Pipaslot.Mediator.Http;
 
 namespace Demo.Server.Handlers.Playground;
 
-public class DemoDownloadHandler : IMediatorHandler<DemoDownload, IMediatorHttpResult>
+public class DemoDownloadHandler : IMediatorHandler<DemoDownload, DemoDownloadResult>
 {
-    public Task<IMediatorHttpResult> Handle(DemoDownload action, CancellationToken cancellationToken)
+    public Task<DemoDownloadResult> Handle(DemoDownload action, CancellationToken cancellationToken)
     {
-        return Task.FromResult<IMediatorHttpResult>(new DemoDownloadResult(action.FileName, "Hello File!"));
-    }
-}
-
-internal class DemoDownloadResult(string fileName, string content) : IMediatorHttpResult
-{
-    public Task ApplyAsync(HttpContext httpContext, CancellationToken cancellationToken)
-    {
-        httpContext.Response.ContentType = "text/plain";
-        httpContext.Response.Headers["Content-Disposition"] = $"attachment;filename={fileName}.txt";
-        return httpContext.Response.WriteAsync(content, cancellationToken);
+        return Task.FromResult(new DemoDownloadResult(action.FileName, "Hello File!"));
     }
 }
